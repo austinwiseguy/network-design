c48b4457f6db758bb9ef5d761f6ea856daf78640
austinwiseguy <92062922+austinwiseguy@users.noreply.github.com>
2022-10-27 13:01:54 -0400

    Update UDPserver.py

--- UDPserver.py
+++ UDPserver.py
@@ -11,39 +11,57 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)                  # sets up server soc
 serverSocket.bind(('', serverPort))                         # assigns port number to the server's socket
 
 # image = input("Enter location and image to write data to: ")             # prompt user to enter image location
-image = r"C:\Users\awise\Desktop\image.jpeg"
+image = r"C:\Users\awise\Desktop\image.bmp"
 random.seed()
 
 print("ready to receive...")    # waiting
 file = open(image, "wb")        # open new image to write
 i = 0
+ack = b'0'
 
 
 # while connection is open
 while True:
-    segAndAddr = serverSocket.recvfrom(10)
+    segAndAddr = serverSocket.recvfrom(12)
     packet_data = segAndAddr[0]
     clientAddress = segAndAddr[1]
 
     if packet_data:
         while packet_data:
-            checksum = list(packet_data[-8:])               # eventually use to compute checksum
-            msg = packet_data[:2]
-            print(packet_data)
+            seq = packet_data[:1]                                       # first byte of packet is sequence num
+            if ack == seq:
+                checksum = list(packet_data[-8:])                       # last eight bytes are checksum
+                # will compute checksum here
+                # --------------------------
+                # read 2 bytes of image data
+                msg = packet_data[1:3]                                  # bytes 2-3 are image data
+                file.write(msg)                                         # write to file
+                send_packet = header.make_packet(ack, seq, checksum)    # assemble ack packet
+                serverSocket.sendto(send_packet, clientAddress)         # send ack to client
+                ack = header.flip_ack(ack)                              # flip ack to next packet
+            else:
+                serverSocket.sendto(send_packet, clientAddress)         # resend ack to client
 
-            file.write(msg)                                 # write to file
-            packet_data = serverSocket.recv(10)
-            print(i)
+            packet_data = serverSocket.recv(12)                         # receive next packet from client
             i += 1
     else:
         break
 
     break
 
-serverSocket.sendto(str(i).encode(), clientAddress)
+serverSocket.sendto(str(i).encode(), clientAddress)         # arbitrary
 
 serverSocket.close()                                        # close server socket
 print("file written successfully")
+
+
+
+
+
+
+
+
+
 
 
 
