a232b0e434b85fab731e74444852bedce6f6787f
austinwiseguy <92062922+austinwiseguy@users.noreply.github.com>
2022-10-27 13:02:47 -0400

    Update UDPclient.py

--- UDPclient.py
+++ UDPclient.py
@@ -7,18 +7,21 @@ import numpy as np
 import header
 
 # serverName = input("Enter IP Address of server: ")  # get ip address of server from user
-serverName = ""
+serverName = "10.250.1.66"
 serverPort = 12000  # server port
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)  # underlying network uses ipv4 address, creates UDP socket
 
 # img = input("Enter location of image to send to server: ")  # get location and name of image
-img = r"C:\Users\awise\Desktop\jpeg_image.jpeg"
+img = r"C:\Users\awise\Desktop\diode.bmp"
 
 # type_error = header.error_path()
 type_error = 0
 msg = 1
+x = 1
 i = 0
+seq = b'0'
+
 
 try:
     transfer_file = open(img, 'rb')         # opens user image for reading binary
@@ -26,18 +29,25 @@ try:
 
     # while the packet is not empty, the packet gets sent to the server to write to the new image
     while msg:
-        checksum = header.check_sum(msg)
-        send_packet = header.make_packet(msg, checksum)
-        # sndpkt = header.make_packet(ack, packet, checksum)
-        i += 1
-        clientSocket.sendto(send_packet, (serverName, serverPort))  # send data over UDP socket
-        msg = transfer_file.read(2)
+        checksum = header.check_sum(msg)                                # compute checksum
+
+        send_packet = header.make_packet(seq, msg, checksum)            # make packet
+        clientSocket.sendto(send_packet, (serverName, serverPort))      # send data over UDP socket
 
         # now wait for ACK from server
-        # while clientSocket.recv(2048) != 'ack':
-        #     print("waiting...")
+        x = clientSocket.recv(12)
+        ack = x[:1]
 
-        # print("Sending next packet!")
+        # if seq and ack are not equal, resend packet over UDP socket
+        if seq != ack:
+            clientSocket.sendto(send_packet, (serverName, serverPort))
+        else:
+            seq = header.flip_ack(seq)                                  # flip seq number bit to reflect next packet
+
+        i += 1
+
+        # read next bytes from image
+        msg = transfer_file.read(2)
 
 # error handling
 except FileNotFoundError:
@@ -50,8 +60,6 @@ finally:
 return_msg, serverAddress = clientSocket.recvfrom(2048)     # receives confirmation message from server
 clientSocket.close()                                        # close the socket
 
-# bmp_size = int(int(bmp_size) / 1024)                        # calculation to convert size of file to kb
-
-print("File of size " + str(bmp_size) + "kb written to new image")    # prints completion statement
+print("Successfully copied image to server!")               # prints completion statement
 
 
