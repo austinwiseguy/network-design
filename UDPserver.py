3e3918a52eb344de2aaa06b17b7f71ea10bff5e6
austinwiseguy <92062922+austinwiseguy@users.noreply.github.com>
2022-10-28 16:01:33 -0400

    Update UDPserver.py

--- UDPserver.py
+++ UDPserver.py
@@ -31,14 +31,16 @@ while True:
             seq = packet_data[:1]                                       # first byte of packet is sequence num
             if ack == seq:
                 checksum = list(packet_data[-8:])                       # last eight bytes are checksum
+                msg = packet_data[0:4]                                  # bytes 2-3 are image data
+                server_checksum = header.check_sum(msg)
+                flip = header.one_comp(server_checksum)
+                add = header.add(flip, server_checksum, ack)            # adds checksum to verify, flips ack if correct
                 # will compute checksum here
                 # --------------------------
                 # read 2 bytes of image data
-                msg = packet_data[1:3]                                  # bytes 2-3 are image data
                 file.write(msg)                                         # write to file
                 send_packet = header.make_packet(ack, seq, checksum)    # assemble ack packet
                 serverSocket.sendto(send_packet, clientAddress)         # send ack to client
-                ack = header.flip_ack(ack)                              # flip ack to next packet
             else:
                 serverSocket.sendto(send_packet, clientAddress)         # resend ack to client
 
@@ -49,7 +51,7 @@ while True:
 
     break
 
-serverSocket.sendto(str(i).encode(), clientAddress)         # arbitrary
+# serverSocket.sendto(str(i).encode(), clientAddress)         # arbitrary
 
 serverSocket.close()                                        # close server socket
 print("file written successfully")
